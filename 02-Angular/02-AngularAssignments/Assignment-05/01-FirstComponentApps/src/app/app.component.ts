729e10b0c2959c3ca99b247958c3f5b26dd8180c
shekhar73 <somshekhar@xecurify.com>
2023-12-27 22:55:29 +0530

    Modified assignment 5

--- 02-Angular/02-AngularAssignments/Assignment-05/01-FirstComponentApps/src/app/app.component.ts
+++ 02-Angular/02-AngularAssignments/Assignment-05/01-FirstComponentApps/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FirstValueFromConfig } from 'rxjs/internal/firstValueFrom';
-import { FirstCompComponent } from './first-comp/first-comp.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FirstCompComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
