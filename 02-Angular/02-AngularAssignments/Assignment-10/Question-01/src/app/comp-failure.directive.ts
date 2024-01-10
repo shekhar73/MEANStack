236992d75bbc50a2575dca632f420b22597d1522
shekhar73 <somshekhar@xecurify.com>
2024-01-11 00:28:22 +0530

    Assignment number-10

---
+++ 02-Angular/02-AngularAssignments/Assignment-10/Question-01/src/app/comp-failure.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appCompFailure]',
+  standalone: true
+})
+export class CompFailureDirective {
+
+  constructor(private obj : ElementRef) 
+  { 
+
+  }
+ 
+  @HostListener('mouseenter') onmouseenter()
+  {
+    this.obj.nativeElement.style.background = 'red';
+  }
+
+  @HostListener('mouseleave') onmouseleave()
+  {
+    this.obj.nativeElement.style.background = 'pink';
+  }
+}
